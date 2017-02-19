f9b8cefa8b4d445cac8574ce60e7fa1f62e96cb7
yizhong <chenyi@lafayette.edu>
2017-02-19 16:06:43 -0500

    adding decode for status line

--- Project1/HTTPResponseDecoder.java
+++ Project1/HTTPResponseDecoder.java
@@ -1,10 +1,43 @@
+import java.util.*;
+import java.nio.ByteBuffer;
 /**
  * Created by nortondj on 2/19/17.
  */
 public class HTTPResponseDecoder {
-
-    public byte[] decode(byte[] response){
-        return new byte[0];
+    float version;
+    int sp, cr, lf;
+    int status_code;
+    String phrase;
+    String header;
+    String value;
+    String body;
+    
+    public float decode(byte[] response){
+        //the byte array received: Version|sp|status|sp|phrase|cr|lf|
+        //                         header|sp|value|cr|lf|
+        //                         header|sp|value|cr|lf|
+        //                         cr|lf|
+        //                         Entire body
+        
+        //byte array -> float
+        return ByteBuffer.wrap(response).getFloat();
     }
-
+    
+    public byte[] toByteArray(float value) {    
+        ByteBuffer buffer = ByteBuffer.allocate(4);
+        buffer.putFloat(value);
+        return buffer.array();
+        
+    }
+    
+    public static void main(String[] args){
+        float version1 = 1.0f;
+        HTTPResponseDecoder h1 = new HTTPResponseDecoder();
+        byte[] array1 = h1.toByteArray(version1);
+        //System.out.println(array1[0]);
+        //System.out.println(array1[1]);
+        //System.out.println(array1[2]);
+        //System.out.println(array1[3]);
+        System.out.println(h1.decode(array1));
+    }
 }
