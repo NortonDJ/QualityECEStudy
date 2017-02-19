1f388a8661db9368d40c5c9902b0fb41b5f3d3eb
nortondj <nortondj@lafayette.edu>
2017-02-19 02:10:38 -0500

    Refactored main into different methods

--- Project1/ServerApp.java
+++ Project1/ServerApp.java
@@ -1,25 +1,23 @@
-import com.sun.corba.se.spi.activation.Server;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 //This class represents the server application
 public class ServerApp
 {
-    private Calendar cal;
     private DateFormat format;
+    private TransportLayer transportLayer;
+    private int dprop;  // ms
+    private int dtrans; // ms per byte
 
     public static void main(String[] args) throws Exception
     {
-        int dprop;  // ms
-        int dtrans; // ms per byte
+        int dprop;  //ms
+        int dtrans; //ms per byte
         switch(args.length){
             case 1 :
                 System.out.println("Invalid arguments: requires specifying a " +
                         "dprop AND a dtrans.");
-                dprop = 1000; dtrans = 200;
+                dprop = 100; dtrans = 20;
                 System.out.println("Setting dprop = " + dprop +
                         " and dtrans = " + dtrans);
                 break;
@@ -29,42 +27,50 @@ public class ServerApp
                     dtrans = Integer.parseInt(args[1]);
                 } catch(Exception e){
                     System.out.println(e.getMessage());
-                    dprop = 1000; dtrans = 200;
+                    dprop = 100; dtrans = 20;
                     System.out.println("Setting dprop = " + dprop +
                             " and dtrans = " + dtrans);
                 }
-            default: dprop = 1000; dtrans = 200;
+            default: dprop = 100; dtrans = 20;
         }
-        ServerApp s = new ServerApp();
-
-
-
-        //create a new transport layer for server (hence true) (wait for client)
-        TransportLayer transportLayer = new TransportLayer(true, dprop, dtrans);
-
-
-        while( true )
-        {
-            //receive message from client, and send the "received" message back.
-            byte[] byteArray = transportLayer.receive();
-            //if client disconnected
-            if(byteArray==null)
-                break;
-            String str = new String ( byteArray );
-            System.out.println( str );
-            String line = s.getCurTime() + "received";
-            byteArray = line.getBytes();
-            transportLayer.send( byteArray );
-        }
+        ServerApp s = new ServerApp(dprop, dtrans);
+        s.run();
     }
 
-    public ServerApp(){
-        this.cal = Calendar.getInstance();
+    public ServerApp(int dprop, int dtrans){
         this.format = new SimpleDateFormat("MM/dd/yy HH:mm:ss");
         System.out.println(getCurTime());
+        this.dprop = dprop;
+        this.dtrans = dtrans;
+        transportLayer = new TransportLayer(true, dprop, dtrans);
     }
 
     public String getCurTime(){
+        Calendar cal = Calendar.getInstance();
         return (format.format(cal.getTime()));
+    }
+
+    public void sendMessage(String message){
+        byte[] byteArray = message.getBytes();
+        transportLayer.send( byteArray );
+    }
+
+    public byte[] receiveMessage(){
+        //receive message from client, and send the "received" message back.
+        byte[] byteArray = transportLayer.receive();
+        return byteArray;
+    }
+
+    public void run(){
+        while( true )
+        {
+            byte[] bytes = receiveMessage();
+            if(bytes==null)
+                break;
+            String str = new String ( bytes );
+            System.out.println( str );
+            String line = getCurTime() + "received";
+            sendMessage(line);
+        }
     }
 }
