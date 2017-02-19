2c2273889533021ae0bb9897d1951e989b84001a
nortondj <nortondj@lafayette.edu>
2017-02-19 14:16:56 -0500

    Added http classes and modified client app to include them

---
+++ Project1/HTTPRequestBuilder.java
@@ -0,0 +1,17 @@
+/**
+ * HTTP class which deals with all headings, append messages to locations of files
+ */
+public class HTTPRequestBuilder
+{
+    private static final int[] urlBits = {};
+    private static final int[] versionBits = {};
+    private static final int[] statusCodeBits = {};
+
+    /**
+     * Constructor for objects of class HTTP
+     */
+    public HTTPRequestBuilder(){
+
+    }
+
+}
---
+++ Project1/HTTPResponseBuilder.java
@@ -0,0 +1,5 @@
+/**
+ * Created by nortondj on 2/19/17.
+ */
+public class HTTPResponseBuilder {
+}
