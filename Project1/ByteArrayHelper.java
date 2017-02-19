21bf7a67dc21a300b852c1c66f78d6601495fa46
yizhong <chenyi@lafayette.edu>
2017-02-19 17:48:36 -0500

    arrayhelper added more

--- Project1/ByteArrayHelper.java
+++ Project1/ByteArrayHelper.java
@@ -4,13 +4,28 @@ import java.nio.ByteBuffer;
  * Created by nortondj on 2/19/17.
  */
 public class ByteArrayHelper {
-
+    public static final byte LF = 12;
+    public static final byte CR = 15;
+    public static final byte SP = 16;
+    
     public static byte[] toByteArray(float value) {
         ByteBuffer buffer = ByteBuffer.allocate(4);
         buffer.putFloat(value);
         return buffer.array();
     }
+    
+    public static int toInt(byte[] bytes){
+        return ByteBuffer.wrap(bytes).getInt();
+    }
 
+    public static char toChar(byte[] bytes){
+        return ByteBuffer.wrap(bytes).getChar();
+    }
+    
+    public static String toString(byte[] bytes){
+        return bytes.toString();
+    }
+    
     public static float toFloat(byte[] bytes){
         return ByteBuffer.wrap(bytes).getFloat();
     }
