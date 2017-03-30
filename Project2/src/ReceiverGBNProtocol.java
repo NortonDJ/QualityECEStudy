96dcd0368e0c45f6db31e244e9c1c97705f39a92
nortondj <nortondj@lafayette.edu>
2017-03-30 08:39:36 -0400

    Add templates for Receiver Transport subclasses

---
+++ Project2/src/ReceiverGBNProtocol.java
@@ -0,0 +1,25 @@
+/**
+ * Created by nortondj on 3/30/17.
+ */
+public class ReceiverGBNProtocol extends ReceiverTransport {
+
+    public ReceiverGBNProtocol(NetworkLayer nl, ReceiverApplication ra, int windowSize) {
+        super(nl, ra, windowSize);
+    }
+
+    public void initialize() {
+
+    }
+
+    public void sendMessage(Message msg) {
+
+    }
+
+    public void receiveMessage(Packet pkt) {
+
+    }
+
+    public void timerExpired() {
+
+    }
+}
---
+++ Project2/src/ReceiverTCPProtocol.java
@@ -0,0 +1,26 @@
+/**
+ * Created by nortondj on 3/30/17.
+ */
+public class ReceiverTCPProtocol extends ReceiverTransport {
+
+    public ReceiverTCPProtocol(NetworkLayer nl, ReceiverApplication ra, int windowSize) {
+        super(nl, ra, windowSize);
+    }
+
+    public void initialize() {
+
+    }
+
+    public void sendMessage(Message msg) {
+
+    }
+
+    public void receiveMessage(Packet pkt) {
+
+    }
+
+    public void timerExpired() {
+
+    }
+
+}
