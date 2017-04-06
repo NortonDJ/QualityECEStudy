24918bf8c5eed70fbd8dcf44357a71c7db056e3c
nortondj <nortondj@lafayette.edu>
2017-04-06 18:39:29 -0400

    Add ReceiverTCPProtocol SendMessage implementation

--- Project2/src/ReceiverTCPProtocol.java
+++ Project2/src/ReceiverTCPProtocol.java
@@ -2,13 +2,17 @@
  * Created by nortondj on 3/30/17.
  */
 public class ReceiverTCPProtocol extends ReceiverTransport {
+    public int getExpectedSeqNum() {
+        return expectedSeqNum;
+    }
+    private int expectedSeqNum;
 
     public ReceiverTCPProtocol(NetworkLayer nl, ReceiverApplication ra, int windowSize) {
         super(nl, ra, windowSize);
     }
 
     public void initialize() {
-
+        expectedSeqNum = 0;
     }
 
     public void sendMessage(Message msg) {
@@ -16,8 +20,23 @@ public class ReceiverTCPProtocol extends ReceiverTransport {
     }
 
     public void receiveMessage(Packet pkt) {
-
+        System.out.println("RECEIVER TCP RECEIVED:  " + pkt.toString());
+        if(verifyPacket(pkt) && pkt.getSeqnum() == expectedSeqNum){
+            Message msg = pkt.getMessage();
+            ra.receiveMessage(msg);
+            sendAck(expectedSeqNum);
+            expectedSeqNum++;
+        } else {
+            sendAck(expectedSeqNum - 1);
+        }
     }
+
+    public void sendAck(int acknum){
+        Packet ack = new Packet(new Message("I'm an ACK"), -1, acknum, -1);
+        System.out.println("RECEIVER TCP SENDING:   " + ack.toString());
+        nl.sendPacket(ack, to);
+    }
+
 
     public void timerExpired() {
 
