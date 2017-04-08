98bb2aaa985e36923a8739b12196ae06438849bc
nortondj <nortondj@lafayette.edu>
2017-04-07 20:49:50 -0400

    Add ReceiverApplication messagesReceived list for unit testing

--- Project2/src/ReceiverApplication.java
+++ Project2/src/ReceiverApplication.java
@@ -1,12 +1,19 @@
+import java.util.ArrayList;
 
 /**
  * A class which represents the receiver's application. It simply prints out the message received from the tranport layer.
  */
 public class ReceiverApplication
 {
+    private ArrayList<Message> messagesReceived = new ArrayList<Message>();
+
     public void receiveMessage(Message msg)
     {
-        //System.out.println("Receiver received:" + msg.getMessage());
+        System.out.println("Receiver received:" + msg.getMessage());
+        messagesReceived.add(msg);
     }
 
+    public ArrayList<Message> getMessagesReceived() {
+        return messagesReceived;
+    }
 }
