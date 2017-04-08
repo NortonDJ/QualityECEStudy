25c8bcb7c9c0c761da1e5433c421e4d59f563f62
nortondj <nortondj@lafayette.edu>
2017-04-07 22:50:55 -0400

    Add test fixtures for corruption testing

---
+++ Project2/tests/SenderGBNProtocolWithCorruptionTest.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by nortondj on 4/7/17.
+ */
+class SenderGBNProtocolWithCorruptionTest {
+    private ArrayList<String> messageArray;
+    private Timeline tl;
+    private NetworkLayer nl;
+    private SenderGBNProtocol st;
+
+    @BeforeEach
+    void setUp() {
+        String filename = "./src/test.txt";
+        int timeBtwnMsgs = 10;
+        float pLoss = 0;
+        float pCorr = 0;
+        int winSize = 3;
+        int protocol = 0;
+        int debug = 0;
+        int timeOut = 100;
+
+        //reading in file line by line. Each line will be one message
+        messageArray = NetworkSimulator.readFile(filename);
+        //creating a new timeline with an average time between packets.
+        tl = new Timeline(timeBtwnMsgs, messageArray.size());
+        //creating a new network layer with specific loss and corruption probability.
+        nl = new NetworkLayer(pLoss, pCorr, tl);
+        //create the sender transport from the factory
+        st = new SenderGBNProtocol(nl,tl,winSize,100);
+        st.enableCorruption(false);
+    }
+
+}
